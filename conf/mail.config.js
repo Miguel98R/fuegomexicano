fb12e31aefc575b2b2a556b5ba4c4053223c82c8
Miguel Rosas <111320389+mikeleganux1998@users.noreply.github.com>
2024-02-06 11:46:12 -0600

    Update mail.config.js

--- conf/mail.config.js
+++ conf/mail.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
+    service: "gmail",
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
